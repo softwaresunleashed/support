2b70a08f2c654fde72904ecd3564437bc222bd39
Sudhanshu Gupta <Softwares.Unleashed@gmail.com>
2017-06-16 16:49:56 +0530

    Added support/

---
+++ set_env.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+export ARCH=arm
+export CROSS_COMPILE=arm-bcm2708hardfp-linux-gnueabi-
+export PATH=$PATH:/home/sudhanshu/rpi/tools/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin
+
